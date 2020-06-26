7a4496055761720c440b492335cfafe705df5c68
TiborGulyas <gulyastibor@gmail.com>
2020-06-26 02:12:10 +0200

    Header text created

---
+++ src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="main-title">Pokemon App</h1>
+    </header>
+  );
+}

ea82b029bfe9d5841ad2a6a11bd902bf5a65b730
TiborGulyas <gulyastibor@gmail.com>
2020-06-27 23:34:26 +0200

    PokemonProperties  "hooked"

--- src/components/PokemonProperties.js
+++ src/components/PokemonProperties.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./PokemonProperties.css";
 import split from "split-string";
 
-export class PokemonItem extends Component {
-  render() {
-    return this.props.pokemons.map((pokemon) => (
-      <Link
-        className="pokemons"
-        to={`/pokemon/id/${
-          split(pokemon.url, { separator: "/" })[
-            split(pokemon.url, { separator: "/" }).length - 2
-          ]
-        }`}
-        onClick={this.props.currentPokemonName.bind(this, pokemon.name)}
-      >
-        {
-          split(pokemon.url, { separator: "/" })[
-            split(pokemon.url, { separator: "/" }).length - 2
-          ]
-        }
-        _{pokemon.name};
-      </Link>
-    ));
-  }
-}
+const PokemonItem = (props) => {
+  return props.pokemons.map((pokemon) => (
+    <Link
+      className="pokemons"
+      to={`/pokemon/id/${
+        split(pokemon.url, { separator: "/" })[
+          split(pokemon.url, { separator: "/" }).length - 2
+        ]
+      }`}
+      onClick={props.currentPokemonName.bind(null, pokemon.name)}
+    >
+      {
+        split(pokemon.url, { separator: "/" })[
+          split(pokemon.url, { separator: "/" }).length - 2
+        ]
+      }
+      _{pokemon.name};
+    </Link>
+  ));
+};
 
 export default PokemonItem;

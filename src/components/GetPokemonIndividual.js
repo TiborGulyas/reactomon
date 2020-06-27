664441e3f702cac9b7dc5620ba1cd81f2205e39a
TiborGulyas <gulyastibor@gmail.com>
2020-06-28 00:38:59 +0200

    GetPokemonIndividual.js "hooked"

--- src/components/GetPokemonIndividual.js
+++ src/components/GetPokemonIndividual.js
@@ -1,43 +1,32 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export class GetPokemonIndividual extends Component {
-  state = {
-    currentPokemonAbilities: [],
-  };
+const GetPokemonIndividual = (props) => {
+  const [currentPokemonAbilities, setCurrentPokemonAbilities] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     console.log("abilities is running");
-    console.log(this.props.id);
-    var url = "https://pokeapi.co/api/v2/pokemon/".concat(this.props.id);
+    console.log(props.id);
+    var url = "https://pokeapi.co/api/v2/pokemon/".concat(props.id);
     console.log(url);
     axios.get(url).then((res) => {
-      res.data.abilities.map((key) => {
-        console.log(JSON.stringify(key.ability.name));
-        this.setState({
-          currentPokemonAbilities: [
-            ...this.state.currentPokemonAbilities,
-            String(key.ability.name),
-          ],
-        });
-        console.log(this.state.currentPokemonAbilities);
-        return this;
+      const result = res.data.abilities;
+      const setOfAbilities = [];
+      result.map((key) => {
+        setOfAbilities.push(key.ability.name);
       });
+      setCurrentPokemonAbilities(setOfAbilities);
     });
-  }
+  }, []);
 
-  render() {
-    let content = (
-      <React.Fragment>
-        <h1>{this.props.currentPokemonName}</h1>
-        {this.state.currentPokemonAbilities.map((ability) => (
-          <h2>{ability}</h2>
-        ))}
-      </React.Fragment>
-    );
-
-    return content;
-  }
-}
+  return (
+    <React.Fragment>
+      <h1>{props.currentPokemonName}</h1>
+      {currentPokemonAbilities.map((fg, index) => (
+        <h2>{fg}</h2>
+      ))}
+    </React.Fragment>
+  );
+};
 
 export default GetPokemonIndividual;
